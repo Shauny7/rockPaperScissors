4c51cba7db2e42c9d81c59588b13daf66b1aed86
Shaun Longden <shaun.longden@contentdiscovered.com>
2018-06-12 11:55:01 +0100

    feat: new everything!

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rock Paper Scissors!</title>
+<!--CSS-->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Yellowtail" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+<!--CSS-->
+</head>
+<body>
+<div class="title-container">      
+    <h1>Rock, Paper, Scissors!</h1>
+</div> 
+</div>
+<div class="button-container">
+    <button class="buttons"><i class="fas fa-hand-rock"></i></button>
+    <button class="buttons"><i class="fas fa-hand-paper"></i></button>
+    <button class="buttons"><i class="fas fa-hand-scissors"></i></button>
+</div>
+</body>
+
+</html>
